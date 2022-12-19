effdabc30eb76f6c6e82ff4f20eb90944a9c77c2
Lena <Lenysik1995@example.com>
2022-12-19 19:10:17 +0500

    Добавлена 1 задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int GetNumberFromUser()
+{
+  Console.Write("Введите число от 100 до 999: ");
+  int temp = int.Parse(Console.ReadLine());
+  return temp;
+}
+
+bool IsValid(int number)
+{
+  return number >= 100 && number <= 999;
+}
+
+int GetNumber(int value)
+{
+  int b = (value / 10) % 10;
+
+  return b;
+}
+
+string GetText(int input, int output)
+{
+  return input + " -> " + output;
+}
+
+void Task10()
+{
+  int value = GetNumberFromUser();
+
+  if (IsValid(value))
+  {
+    int result = GetNumber(value);
+    System.Console.WriteLine(GetText(value, result));
+  }
+  else
+  {
+    System.Console.WriteLine("Число не трехзначное");
+  }
+}
+Task10();
+
+
+
+
+
+
+
